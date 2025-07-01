ababc3925feaa19f4fd6d74727b9f13eefd8f6c5
Shan <rc.shanchaidiane.leones@cvsu.edu.ph>
2025-07-01 11:19:39 +0800

    Update connect.php

--- components/connect.php
+++ components/connect.php
@@ -2,15 +2,16 @@
 
 $db_host = getenv("DB_HOST");
 $db_name = getenv("DB_NAME");
-$user_name = getenv("DB_USER");
-$user_password = getenv("DB_PASS");
+$db_user = getenv("DB_USER");
+$db_pass = getenv("DB_PASS");
 
 $dsn = "mysql:host=$db_host;dbname=$db_name;charset=utf8mb4";
 
 try {
-    $conn = new PDO($dsn, $user_name, $user_password);
+    $conn = new PDO($dsn, $db_user, $db_pass);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // echo "✅ Connected to DB!";
 } catch(PDOException $e) {
-    die("Connection failed: " . $e->getMessage());
+    die("❌ Connection failed: " . $e->getMessage());
 }
 ?>
