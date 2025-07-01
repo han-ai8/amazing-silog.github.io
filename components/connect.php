7539c1f099f55ba4ac6a7674750abaea3165218a
Shan <rc.shanchaidiane.leones@cvsu.edu.ph>
2025-07-01 11:12:26 +0800

    Update connect.php

--- components/connect.php
+++ components/connect.php
@@ -1,11 +1,14 @@
 <?php
 
-$db_name = 'mysql:host=localhost;dbname=food_db';
-$user_name = 'root';
-$user_password = '';
+$db_host = getenv("DB_HOST");
+$db_name = getenv("DB_NAME");
+$user_name = getenv("DB_USER");
+$user_password = getenv("DB_PASS");
+
+$dsn = "mysql:host=$db_host;dbname=$db_name;charset=utf8mb4";
 
 try {
-    $conn = new PDO($db_name, $user_name, $user_password);
+    $conn = new PDO($dsn, $user_name, $user_password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
     die("Connection failed: " . $e->getMessage());
